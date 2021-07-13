c3434105459abbde66131f81754e19c409680ece
Josh Cowan <cowanjzc@gmail.com>
2021-07-13 12:48:46 -0400

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,11 +9,15 @@ const Button = ({label, clickEvent}) => {
 }
 
 const Stats = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
   return (
     <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / (all)}</p>
+      <p>positive {(good / (all)) * 100}%</p>
     </>
   )
 }
