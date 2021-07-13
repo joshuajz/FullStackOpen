6b94beb2f2495652ae2a885dd89ff2945aa5f00b
Josh Cowan <cowanjzc@gmail.com>
2021-07-13 18:21:49 -0400

    2.2

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const Course = ({course}) => {
+    const exercises = course.parts.map(part => part.exercises)
+    var totalExercises = 0
+    for (let i = 0; i < exercises.length; i++) {
+        totalExercises += exercises[i]
+    }
+
     return (
     <div>
         <h1>{course.name}</h1>
         {course.parts.map(part => <p>{part.name} {part.exercises}</p>)}
+        <p>Total Exercises: {totalExercises}</p>
     </div>
     )
 }
