0848dabee72bc88c51864d2b743961f261a822cc
Josh Cowan <cowanjzc@gmail.com>
2021-08-10 21:19:48 -0400

    4.2

---
+++ part4/app.js
@@ -0,0 +1,22 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to: ', config.MONGODB_URL)
+
+mongoose.connect(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then(() => {
+    logger.info("Connected to MongoDB!")
+}).catch(error => {
+    logger.error('Error connecting to MongoDB.', error.message)
+})
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
