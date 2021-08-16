0739a1e34a08844a7f91d359a8bf3c5acdf584eb
Josh Cowan <cowanjzc@gmail.com>
2021-08-16 19:54:20 -0400

    4.8

--- part4/app.js
+++ part4/app.js
@@ -9,9 +9,9 @@ const mongoose = require('mongoose')
 logger.info('Connecting to: ', config.MONGODB_URL)
 
 mongoose.connect(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then(() => {
-    logger.info("Connected to MongoDB!")
+  logger.info("Connected to MongoDB!")
 }).catch(error => {
-    logger.error('Error connecting to MongoDB.', error.message)
+  logger.error('Error connecting to MongoDB.', error.message)
 })
 
 app.use(cors())
---
+++ part4/tests/note_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initalNotes = [{'title': 'Google\'s best images.', 'author': 'John Sparrow', 'likes': 10}, {'title': 'A Guide to Databases.', 'author': 'John Sparrow', 'likes': 19}, {'title': 'How to purchase from Amazon - A Guide for Seniors', 'author': 'Tim Apple', 'likes': 6}]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initalNotes[0])
+  await blogObject.save()
+  blogObject = new Blog(initalNotes[1])
+  await blogObject.save()
+  blogObject = new Blog(initalNotes[2])
+  await blogObject.save()
+})
+
+test('Connection to the database.', async () => {
+  await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+})
+
+test('Returns the proper amount of notes.', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initalNotes.length)
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
