62b622269f4e163a65a889809fefca5ee1d8a6d6
Josh Cowan <cowanjzc@gmail.com>
2022-01-04 20:57:06 -0500

    4.20

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require("../models/blog")
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization")
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get("/", async (request, response) => {
   const fetch = await Blog.find({}).populate("user")
   response.json(fetch)
@@ -18,7 +10,7 @@ blogsRouter.get("/", async (request, response) => {
 
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body
-  const token = getTokenFrom(request)
+  const token = body.token
   const decodedToken = jwt.verify(token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token missing or invalid" })
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,13 +1,23 @@
 const errorHandler = (error, request, response, next) => {
-  console.log('##Error: ', error.name, ':', error.message)
+  console.log("##Error: ", error.name, ":", error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
     return response.status(400).send({ error: error.message })
   }
 
   next(error)
 }
 
-module.exports = {errorHandler}
+const getToken = (request, response, next) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.tooLowerCase().startsWith("bearer ")) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
+  next()
+}
+
+module.exports = { errorHandler, getToken }
