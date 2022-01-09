207826e4bb3a1a7e541b8e021afdba59826b709f
Josh Cowan <cowanjzc@gmail.com>
2022-01-09 12:11:39 -0500

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisibility] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const toggleVisibility = () => {
+    setVisibility(!visible)
+  }
+
+  const showWhenVisible = { display: visible ? "" : "none" }
+  const hideWhenVisible = { display: visible ? "none" : "" }
+
+  return (
+    <div>
+      <div style={blogStyle}>
+        <div style={hideWhenVisible}>
+          {blog.title} <button onClick={toggleVisibility}>view</button>
+        </div>
+        <div style={showWhenVisible}>
+          {blog.title} <button onClick={toggleVisibility}>hide</button> <br />
+          {blog.url} <br />
+          likes {blog.likes} <button>like</button> <br />
+          {blog.author}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
